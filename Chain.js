5ba322683ff8ad9774aa3ffcde6c2fc0cc1a7eb0
Account1234-hash <70168235+Account1234-hash@users.noreply.github.com>
2020-10-08 21:07:05 -0600

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,21 @@
+class Chain {
+    constructor(bodyA, bodyB) {
+        var options = {
+            bodyA: bodyA,
+            bodyB: bodyB,
+            length: 10,
+            stiffness: 0.09
+        }
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+
+        display(){
+            var PointA = this.chain.bodyA.position;
+            var PointB = this.chain.bodyB.position;
+            strokeWeight(3);
+            line(PointA.x, PointA.y, PointB.x, PointB.y);
+        }
+    }
+  
+    
